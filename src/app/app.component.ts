f692b5d6a7f8fe7bff7599deaf060a92a5ef444b
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2022-06-05 20:18:32 +0300

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'SognoNft';
+  
 }
